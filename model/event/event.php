67e9b8ffc55881fb536efd33881105ac01374288
Shane Moe <ssoemoe@terpmail.umd.edu>
2018-09-16 05:18:57 -0400

    create event feature added

--- model/event/event.php
+++ model/event/event.php
@@ -7,12 +7,21 @@ class Event {
     public $type;
     public $status;
     public $location;
+    public $attendees;
+    public $organizers;
     
     private $conn;
     private $table_name = "events";
     
     public function __construct($db) {
         $this->conn = $db;
+    }
+    
+    public function createEvent($data) {
+        $query = "insert into ".$this->table_name." (id, name, time, description, type, status, location)
+        values (default, :n, :time, :des, :ty, :stat, :loc)";
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute($data);
     }
 }
 ?>
